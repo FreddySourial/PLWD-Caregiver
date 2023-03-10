0d18ef6d0bcbb6712f8ba46623ee82aa578a9380
Freddy Sourial <freddy.sourial@gmail.com>
2023-03-10 00:08:57 -0500

    Initial Commit

---
+++ PLWD Caregiver/PLWD_CaregiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLWD_CaregiverApp.swift
+//  PLWD Caregiver
+//
+//  Created by Freddy Sourial on 2023-03-10.
+//
+
+import SwiftUI
+
+@main
+struct PLWD_CaregiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
